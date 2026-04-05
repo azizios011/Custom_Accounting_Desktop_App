9869c567c1ed55148478a6e340a530c596bee499
azizios011 <hdriaziz699@gmail.com>
2026-04-05 20:25:52 +0100

    adocker.docker.V.45

--- remove-erp.bat
+++ remove-erp.bat
@@ -2,5 +2,14 @@
 REM Force remove the container named erp-app, ignore errors if not found
 docker rm -f erp-app 2>nul
 
+REM Recursively search for symlinks named "public" up to 10 levels deep and delete them
+for /r %%i in (public) do (
+    if exist "%%i" (
+        echo Deleting symlink: %%i
+        rmdir "%%i" 2>nul
+        del "%%i" 2>nul
+    )
+)
+
 REM Pause so you can see the result
 pause
